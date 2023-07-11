eb870f32e5406dac256e05d80bf2ff12e706d972
Samuel <enthunt.sh@gmail.com>
2023-07-11 10:27:32 -0400

    added mobile navbar added diable scrolling functionality

--- src/scripts/navbarFunction.js
+++ src/scripts/navbarFunction.js
@@ -1,6 +1,46 @@
-var mobileNavBtn = document.getElementById('mobile-nav-btn');
-var mobileNavMenu = document.getElementById('mobile-nav-menu');
-mobileNavBtn.addEventListener('click', function () {
-    mobileNavMenu.classList.add('activate');
-    console.log("hi");
-});
+let mobileNavBtn = document.getElementById('mobile-nav-btn');
+let mobileNavMenu = document.getElementById('mobile-nav-menu');
+let mobileNavExitBtn = document.getElementById('mobile-nav-exit');
+
+
+/* mobileNavBtn.addEventListener('click', ()=> {
+    mobileNavMenu.style.width = '100%';
+    mobileNavBtn.style.display = 'none';
+    mobileNavExitBtn.style.display = 'block'; //
+    
+    mobileNavBtn.style.opacity = '0'; 
+})
+ */
+
+window.addEventListener('click', (element)=> {
+    if (element.target === mobileNavBtn) { 
+        mobileNavMenu.style.width = '100%';
+        mobileNavBtn.style.display = 'none';
+        mobileNavExitBtn.style.display = 'block';
+        disableScroll();
+    } else if (element.target === mobileNavExitBtn) {
+        mobileNavExitBtn.style.display = 'none';
+        mobileNavMenu.style.width = '0%';
+        mobileNavBtn.style.display = 'block';
+        enableScroll();
+    }  
+})
+
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop =
+    window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft =
+    window.pageXOffset || document.documentElement.scrollLeft,
+  
+        // if any scroll is attempted,
+        // set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+
+
+function enableScroll() {
+    window.onscroll = function() {};
+}
--- src/scripts/navbarFunction.ts
+++ src/scripts/navbarFunction.ts
@@ -1,8 +1,1 @@
 
-let mobileNavBtn : any = document.getElementById('mobile-nav-btn');
-let mobileNavMenu : any = document.getElementById('mobile-nav-menu');
-
-mobileNavBtn.addEventListener('click', ()=> {
-    mobileNavMenu.classList.add('activate');
-    console.log("hi");
-})
