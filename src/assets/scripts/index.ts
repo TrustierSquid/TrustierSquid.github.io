4492721e5f4b8626281809ac4d2f22b0b48c58ca
Samuel <enthunt.sh@gmail.com>
2023-07-26 23:49:23 -0400

    added emailjs functionality

--- src/assets/scripts/index.ts
+++ src/assets/scripts/index.ts
@@ -1,3 +1,31 @@
 
-type specialNumber = string | boolean
-let p1: specialNumber = "sam"
+import emailjs from '@emailjs/browser';
+// import 'dotenv/config'
+
+const templateID: string = 'template_2nkyimf'
+const serviceID: string = 'service_47zc9zm'
+
+let sendTicket = document.getElementById('send-email');
+sendTicket?.addEventListener('click', init)
+
+function init():void {
+    emailjs.init('fe27j37HF7dPw5soA');
+
+    let params: {firstName: any, lastName: any, email: any, message: any} = {
+        firstName: document.getElementById('contact-fname'),
+        lastName: document.getElementById('contact-lname'),
+        email: document.getElementById('contact-email'),
+        message: document.getElementById('contact-message'),
+    }
+    
+    
+    emailjs.send(serviceID, templateID, params)
+        .then((res) => {
+            alert("your message was sent");
+            console.log(res);
+        }).catch((err) => {
+            console.log(err)
+        })
+}
+
+
