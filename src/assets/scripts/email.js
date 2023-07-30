cdce816bd60f3da71aeb580c0a7a4dd73e772f24
Samuel <enthunt.sh@gmail.com>
2023-07-30 15:18:55 -0400

    optimized buttons

--- src/assets/scripts/email.js
+++
@@ -1,2 +0,0 @@
-import emailjs from '@emailjs/browser'
-
--- src/assets/scripts/index.ts
+++ src/assets/scripts/index.ts
@@ -1,31 +1,3 @@
 
-import emailjs from '@emailjs/browser';
-// import 'dotenv/config'
-
-const templateID: string = 'template_2nkyimf'
-const serviceID: string = 'service_47zc9zm'
-
-let sendTicket = document.getElementById('send-email');
-sendTicket?.addEventListener('click', init)
-
-function init():void {
-    emailjs.init('fe27j37HF7dPw5soA');
-
-    let params: {firstName: any, lastName: any, email: any, message: any} = {
-        firstName: document.getElementById('contact-fname'),
-        lastName: document.getElementById('contact-lname'),
-        email: document.getElementById('contact-email'),
-        message: document.getElementById('contact-message'),
-    }
-    
-    
-    emailjs.send(serviceID, templateID, params)
-        .then((res) => {
-            alert("your message was sent");
-            console.log(res);
-        }).catch((err) => {
-            console.log(err)
-        })
-}
-
-
+let heroGHbutton = document.getElementById('hero-github');
+heroGHbutton?.addEventListener('click', ():void => {window.location.href = 'https://github.com/TrustierSquid'})
