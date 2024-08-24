a65fd519c8566467385f67e1832b742a679d3670
Sam <enthunt.sh@gmail.com>
2024-08-24 01:48:13 -0400

    removed parcel dependency

--- src/assets/scripts/navbarFunction.js
+++ src/assets/scripts/navbarFunction.js
@@ -1,8 +1,8 @@
-const mobileNavBtn = document.getElementById('mobile-nav-btn');
-const mobileNavMenu = document.getElementById('mobile-nav-menu');
-const mobileNavExitBtn = document.getElementById('mobile-nav-exit');
-const navBar = document.getElementById('navbar');
-const mobileNavLinks = document.querySelectorAll('.mobileNavLinks')
+const mobileNavBtn = document.getElementById("mobile-nav-btn");
+const mobileNavMenu = document.getElementById("mobile-nav-menu");
+const mobileNavExitBtn = document.getElementById("mobile-nav-exit");
+const navBar = document.getElementById("navbar");
+const mobileNavLinks = document.querySelectorAll(".mobileNavLinks");
 
 /* mobileNavBtn.addEventListener('click', ()=> {
     mobileNavMenu.style.width = '100%';
@@ -13,103 +13,93 @@ const mobileNavLinks = document.querySelectorAll('.mobileNavLinks')
 })
  */
 
-let redirect = (email)=> {
-    window.location.href = `mailto:${email}`
-}
+let redirect = (email) => {
+  window.location.href = `mailto:${email}`;
+};
 
-let closeMenu = ()=> {
-    mobileNavExitBtn.style.display = 'none';
-    mobileNavMenu.style.width = '0%';
-    mobileNavBtn.style.display = 'block';
-    navBar.style.background = 'none';
-}
+let closeMenu = () => {
+  mobileNavExitBtn.style.display = "none";
+  mobileNavMenu.style.width = "0%";
+  mobileNavBtn.style.display = "block";
+  navBar.style.background = "none";
+};
 
 // MOBILE NAV FUNCTIONALITY
 
-window.addEventListener('click', (element)=> {
-    // Checking for clicking on mobile nav button
-    if (element.target === mobileNavBtn) { 
-        mobileNavMenu.style.width = '100%';
-        mobileNavBtn.style.display = 'none';
-        mobileNavExitBtn.style.display = 'block';
-        location.href = '#'
-        navBar.style.backgroundColor = '#333';
+window.addEventListener("click", (element) => {
+  // Checking for clicking on mobile nav button
+  if (element.target === mobileNavBtn) {
+    mobileNavMenu.style.width = "100%";
+    mobileNavBtn.style.display = "none";
+    mobileNavExitBtn.style.display = "block";
+    location.href = "#";
+    navBar.style.backgroundColor = "#333";
 
+    mobileNavLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        switch (link.innerText) {
+          case "About me":
+            location.href = "#about";
+            closeMenu();
+            console.log("Navigate to " + link.innerText);
+            break;
+          case "Projects":
+            console.log("Navigate to " + link.innerText);
+            location.href = "#projects";
+            closeMenu();
+            break;
+          case "Contact":
+            console.log("Navigate to " + link.innerText);
+            redirect("huntdevelops@gmail.com");
+            closeMenu();
+            break;
+          default:
+        }
+      });
+    });
+  } else if (element.target === mobileNavExitBtn) {
+    closeMenu();
+  }
+});
 
-        mobileNavLinks.forEach((link)=> {
-            link.addEventListener('click', ()=> {
-                switch (link.innerText) {
-                    case "About me":
-                        location.href = '#about'
-                        closeMenu()
-                        console.log("Navigate to " + link.innerText)
-                        break;
-                    case "Projects":
-                        console.log("Navigate to " + link.innerText)
-                        location.href = '#projects'
-                        closeMenu()
-                        break;
-                    case "Contact":
-                        console.log("Navigate to " + link.innerText)
-                        redirect("huntdevelops@gmail.com")
-                        closeMenu()
-                        break;
-                    default:
-                }
-            })
-        })
+function disableScroll() {
+  // Get the current page scroll position in the vertical direction
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-        
-    } else if (element.target === mobileNavExitBtn) {
-        closeMenu()
-    }  
-})
+  // Get the current page scroll position in the horizontal direction
 
-function disableScroll(){
-    // Get the current page scroll position in the vertical direction
-    scrollTop =
-    window.pageYOffset || document.documentElement.scrollTop;
-     
-        
-    // Get the current page scroll position in the horizontal direction 
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
 
-    scrollLeft =
-    window.pageXOffset || document.documentElement.scrollLeft;
-
-
-    // if any scroll is attempted,
-    // set this to the previous value
-    window.onscroll = function() {
+  // if any scroll is attempted,
+  // set this to the previous value
+  window.onscroll = function () {
     window.scrollTo(scrollLeft, scrollTop);
-    }
+  };
 
-    function enableScroll() {
-    window.onscroll = function() {};
-    }
-    
+  function enableScroll() {
+    window.onscroll = function () {};
+  }
 }
-
 
 // * BUTTON FUNCTIONALITY FOR THE HERO SECTION
 
-let projectBtn = document.getElementById('contact-btn');
-let heroGithubBtn = document.getElementById('hero-github')
-let heroLinkedinBtn = document.getElementById('hero-linkedin')
+let projectBtn = document.getElementById("contact-btn");
+let heroGithubBtn = document.getElementById("hero-github");
+let heroLinkedinBtn = document.getElementById("hero-linkedin");
 
-projectBtn.addEventListener('click', ()=> {
-    console.log("clicked project btn")
-    location.href = "#projects"
-})
+projectBtn.addEventListener("click", () => {
+  console.log("clicked project btn");
+  location.href = "#projects";
+});
 
 function migrate(GH, LI) {
-    GH.addEventListener('click', ()=>{
-        location.href = "https://github.com/TrustierSquid";
-    })
+  GH.addEventListener("click", () => {
+    location.href = "https://github.com/TrustierSquid";
+  });
 
-    LI.addEventListener('click', ()=>{
-        location.href = "www.linkedin.com/in/samuel-hunt-387ab7245"
-    })
-
+  LI.addEventListener("click", () => {
+    location.href = "www.linkedin.com/in/samuel-hunt-387ab7245";
+  });
 }
 
-migrate(heroGithubBtn, heroLinkedinBtn)
+migrate(heroGithubBtn, heroLinkedinBtn);
